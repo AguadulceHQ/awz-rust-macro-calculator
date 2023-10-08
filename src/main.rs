044c6505111341c3c3c94defdedcf751e3b3f892
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-10-08 21:19:06 +0700

    feat(person): add base `Person` struct, a couple enums and implement `Display` trait on all types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+use std::fmt;
+
+struct Person {
+    name: String,
+    age: u8,
+    gender: Gender,
+    height: u8,
+    weight: u8,
+    activty: Activity,
+}
+
+enum Gender {
+    Female,
+    Male,
+}
+
+enum Activity {
+    Sedentary,
+    LightlyActive,
+    ModeratelyActive,
+    VeryActive,
+    SuperActive,
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} is a {} years old {}, {} cms tall that weighs {}kg with an activity level of {} 📝",
+            self.name, self.age, self.gender, self.height, self.weight, self.activty
+        )
+    }
+}
+
+impl fmt::Display for Gender {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let gender = match *self {
+            Gender::Male => "male",
+            Gender::Female => "female",
+        };
+
+        write!(f, "{}", gender)
+    }
+}
+
+impl fmt::Display for Activity {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let activity = match *self {
+            Activity::Sedentary => "sedentary",
+            Activity::LightlyActive => "lightly active",
+            Activity::ModeratelyActive => "moderately active",
+            Activity::SuperActive => "super active",
+            Activity::VeryActive => "very active",
+        };
+
+        write!(f, "{}", activity)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let luca = Person {
+        name: String::from("Luca"),
+        age: 33,
+        gender: Gender::Male,
+        height: 173,
+        weight: 70,
+        activty: Activity::SuperActive,
+    };
+
+    println!("{}", luca);
 }
