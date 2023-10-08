b714332fcb1e9cad15216c5166cad6b324533704
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-10-08 22:16:21 +0700

    feat(calories): add fn to calculate caloric baseline used to calculate range to achieve goal

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ struct Person {
     activty: Activity,
 }
 
+struct CaloricIntake {
+    min: u8,
+    max: u8,
+}
+
 enum Gender {
     Female,
     Male,
@@ -68,4 +73,51 @@ fn main() {
     };
 
     println!("{}", luca);
+
+    println!("Luca's caloric treshold is {}", caloric_treshold(luca));
+}
+
+fn caloric_treshold(person: Person) -> f32 {
+    const MEN_MULTIPLIERS: (f32, f32, f32, f32) = (88.362, 13.397, 4.799, 5.677);
+    const FEMALE_MULTIPLIERS: (f32, f32, f32, f32) = (447.593, 9.247, 3.098, 4.330);
+    const ACTIVITY_MULTIPLIERS: (f32, f32, f32, f32, f32) = (1.2, 1.375, 1.55, 1.725, 1.9);
+
+    let basal_metabolic_rate;
+
+    match person.gender {
+        Gender::Male => {
+            basal_metabolic_rate = MEN_MULTIPLIERS.0
+                + MEN_MULTIPLIERS.1 * person.weight as f32
+                + MEN_MULTIPLIERS.2 * person.height as f32
+                - MEN_MULTIPLIERS.3 * person.age as f32;
+        }
+        Gender::Female => {
+            basal_metabolic_rate = FEMALE_MULTIPLIERS.0
+                + FEMALE_MULTIPLIERS.1 * person.weight as f32
+                + FEMALE_MULTIPLIERS.2 * person.height as f32
+                - FEMALE_MULTIPLIERS.3 * person.age as f32;
+        }
+    }
+
+    let activity_multiplier;
+
+    match person.activty {
+        Activity::LightlyActive => {
+            activity_multiplier = ACTIVITY_MULTIPLIERS.0;
+        }
+        Activity::ModeratelyActive => {
+            activity_multiplier = ACTIVITY_MULTIPLIERS.1;
+        }
+        Activity::Sedentary => {
+            activity_multiplier = ACTIVITY_MULTIPLIERS.2;
+        }
+        Activity::SuperActive => {
+            activity_multiplier = ACTIVITY_MULTIPLIERS.3;
+        }
+        Activity::VeryActive => {
+            activity_multiplier = ACTIVITY_MULTIPLIERS.4;
+        }
+    }
+
+    basal_metabolic_rate * activity_multiplier
 }
